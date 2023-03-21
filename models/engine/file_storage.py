0769ab9de9c49944bb20b2f9fb51925ac4d711e2
Bolexzy <boluoyewumi79@gmail.com>
2023-03-21 01:07:23 +0100

    Update file: Update FileStorage
    Add a new public instance method: def delete(self, obj=None): to delete obj from __objects if it’s inside - if obj is equal to None, the method should not do anything
    Update the prototype of def all(self) to def all(self, cls=None) - that returns the list of objects of one type of class. Example below with State - it’s an optional filtering

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,50 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""
+Defines a FileStorage class Module(JSON).
+"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """A class FileStorage that serializes instances to a JSON file
+    and deserializes JSON file to instances
+    Attributes:
+        __file_path (str): The path to the JSON file to save objects.
+        __objects (dict): A dictionary of instantiated objects.
+    """
     __file_path = 'file.json'
     __objects = {}
+    all_classes = {'BaseModel': BaseModel, 'User': User,
+                   'State': State, 'City': City, 'Amenity': Amenity,
+                   'Place': Place, 'Review': Review}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            if cls in self.all_classes:
+                cls = self.all_classes[cls]
+            cls_dict = {}
+            for key, value in FileStorage.__objects.items():
+                if type(value) == cls:
+                    cls_dict[key] = value
+            return cls_dict
         return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it's inside """
+        if obj is not None:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -45,6 +75,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
