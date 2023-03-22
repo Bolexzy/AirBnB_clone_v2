632914e2e66d3c655d75bc6238d292b9aa8239e0
Bolexzy <boluoyewumi79@gmail.com>
2023-03-22 17:16:10 +0100

    Update module: Inherits from SQLAlchemy Base and links to the MySQL table users.

--- models/user.py
+++ models/user.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""
+    Defines a User class that inherits from BaseModel.
+"""
+from models.base_model import (BaseModel, Base)
+from os import getenv
+from sqlalchemy import (Column, String)
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """Represents a class User.
+    Inherits from BaseModel and SQLAlchemy Base
+    and links to the MySQL table users.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+    """
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
