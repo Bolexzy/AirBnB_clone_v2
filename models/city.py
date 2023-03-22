325fe488cdf7f6ecb75a657548b7959041abf681
Bolexzy <boluoyewumi79@gmail.com>
2023-03-22 18:04:23 +0100

    Update city: addes realtionship class attribute "places"

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -19,3 +20,4 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
