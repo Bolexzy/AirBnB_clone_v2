3e2131c049bfec05c37faffb413390f3bf5cce6f
Bolexzy <boluoyewumi79@gmail.com>
2023-04-08 19:15:22 +0100

    [Update file]: curl localhost/hbnb_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Bash script that sets up my web servers for the deployment of web_static.
 
+sudo apt-get -y update
 if ! command -v nginx &> /dev/null; then
     echo "Nginx is not installed, installing now..."
-    sudo apt-get -y update
     sudo apt-get -y install nginx
     echo "Nginx installed successfully"
 else
@@ -27,7 +27,7 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-new_string="server_name _;\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n"
+new_string="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n"
 
 sed -i "s|server_name _;|${new_string}|" /etc/nginx/sites-available/default
 
