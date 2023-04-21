5dd30a1e09324ef7b173fbe2bd5fb5163f23a29d
Bolexzy <boluoyewumi79@gmail.com>
2023-04-21 17:59:36 +0100

    [Fix]: pep-8 style error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-	"""Displays 'Hello HBNB!'"""
-	return "Hello HBNB!"
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host="0.0.0.0", port=5000, debug=None)
