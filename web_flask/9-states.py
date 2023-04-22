443e8f095a86786052b40cef79bd1ac2c83e185a
Bolexzy <boluoyewumi79@gmail.com>
2023-04-23 00:27:55 +0100

    [Fix error]: object not callable on listtype ["state.cities()"]

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -38,7 +38,7 @@ def state(id):
     for state in storage.all('State').values():
         if state.id == id:
             return render_template('9-states.html', state=state)
-    return render_template('9-states.html', state=None)
+    return render_template('9-states.html')
 
 
 if __name__ == "__main__":
